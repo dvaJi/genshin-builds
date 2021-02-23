fe892b25dfc3195ad576c7fca6ba9eaf70e31a3d
dvaJi <francisco_p@outlook.com>
2021-02-23 13:56:02 -0300

    fix: use env variable for gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-Z73BCQ3WM5";
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
