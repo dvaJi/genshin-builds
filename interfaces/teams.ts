ba9eba67972a1c2a95868017a9b1579726b9d96d
dvaJi <francisco_p@outlook.com>
2021-03-04 18:01:03 -0300

    chore: remove all unused components

--- interfaces/teams.ts
+++
@@ -1,7 +0,0 @@
-export interface RecommendedTeams {
-  name: string;
-  code: string;
-  elemental_resonances: string[];
-  characters: string[];
-  has5Stars: boolean;
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 import App from "@components/App";
 import * as gtag from "@lib/gtag";
@@ -23,11 +21,9 @@ function Root(props: AppProps) {
   }, [router.events]);
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <RecoilRoot>
-        <App {...props} />
-      </RecoilRoot>
-    </DndProvider>
+    <RecoilRoot>
+      <App {...props} />
+    </RecoilRoot>
   );
 }
 
