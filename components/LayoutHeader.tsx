62f762b76aea1a605f884c36376406962abdf21b
dvaJi <francisco_p@outlook.com>
2021-03-11 19:41:33 -0300

    fix: add intl to header

--- components/LayoutHeader.tsx
+++ components/LayoutHeader.tsx
@@ -1,34 +1,36 @@
-import { Fragment, memo, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import NavLink from "./NavLink";
-import clsx from "clsx";
+import { memo, useState } from "react";
 import { motion } from "framer-motion";
-import Logo from "./Logo";
+import { useRouter } from "next/router";
 
-interface NavRoutes {
-  name: string;
-  href: string;
-  dropdownMenu?: NavRoutes[];
-}
+import NavLink from "./NavLink";
+import Logo from "./Logo";
+import MobileNav from "./MobileNav";
+import { NavRoutes } from "interfaces/nav-routes";
+import useIntl from "@hooks/use-intl";
 
 const navroutes: NavRoutes[] = [
-  { name: "Characters", href: "/characters" },
-  { name: "Tierlist", href: "/tier-list" },
+  { id: "characters", name: "Characters", href: "/characters" },
+  { id: "tierlist", name: "Tierlist", href: "/tier-list" },
   {
+    id: "database",
     name: "Database",
     href: "/#",
     dropdownMenu: [
-      { name: "Weapons", href: "/weapons" },
-      { name: "Artifacts", href: "/artifacts" },
+      { id: "weapons", name: "Weapons", href: "/weapons" },
+      { id: "artifacts", name: "Artifacts", href: "/artifacts" },
     ],
   },
   // { name: "Team Builder", href: "/comp-builder" },
 ];
 
-const LayoutHeader = () => {
+type Props = {
+  lngDict: Record<string, string>;
+};
+
+const LayoutHeader = ({ lngDict }: Props) => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   const router = useRouter();
+  const [f, fStr] = useIntl(lngDict);
   return (
     <nav className="sticky top-0 h-12 mb-5 z-40 bg-vulcan-800 border-b border-gray-800 shadow-md">
       <div className="flex h-full mb-12 mx-auto container text-gray-400 ">
@@ -48,9 +50,12 @@ const LayoutHeader = () => {
               key={r.href}
               href={r.href}
               route={router.route}
-              dropdownMenu={r.dropdownMenu}
+              dropdownMenu={r.dropdownMenu?.map((d) => ({
+                ...d,
+                name: fStr({ id: d.id, defaultMessage: d.name }),
+              }))}
             >
-              {r.name}
+              {f({ id: r.id, defaultMessage: r.name })}
             </NavLink>
           ))}
         </ul>
@@ -87,7 +92,7 @@ const LayoutHeader = () => {
           </motion.div>
         </div>
       </div>
-      <MobileNav isOpen={isMobileNavOpen} />
+      <MobileNav isOpen={isMobileNavOpen} navroutes={navroutes} f={f} />
     </nav>
   );
 };
@@ -101,71 +106,5 @@ const Path = (props: any) => (
     {...props}
   />
 );
-
-const MobileNav = ({ isOpen }: { isOpen: boolean }) => {
-  const variants = {
-    open: {
-      transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-    },
-    closed: {
-      transition: { staggerChildren: 0.05, staggerDirection: -1 },
-    },
-  };
-
-  const variantsli = {
-    open: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        y: { stiffness: 1000, velocity: -100 },
-      },
-    },
-    closed: {
-      y: 50,
-      opacity: 0,
-      transition: {
-        y: { stiffness: 1000 },
-      },
-    },
-  };
-  return (
-    <motion.div
-      className={clsx(
-        "bg-vulcan-800 text-gray-400 transition-all p-7 flex absolute top-12 left-0 flex-col items-center w-full z-20 opacity-0 pointer-events-none lg:hidden",
-        isOpen ? "opacity-100 pointer-events-auto" : ""
-      )}
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-    >
-      <h2 className="text-lg mb-5">Navigation</h2>
-      <motion.ul
-        className="flex items-center flex-row flex-wrap"
-        variants={variants}
-      >
-        {navroutes.map((r) => (
-          <Fragment key={r.name}>
-            <motion.li className="flex w-1/2 m-0 p-3" variants={variantsli}>
-              <Link href={r.href}>
-                <a className="mobile-link">{r.name}</a>
-              </Link>
-            </motion.li>
-            {r.dropdownMenu &&
-              r.dropdownMenu.map((rd) => (
-                <motion.li
-                  key={rd.name}
-                  className="flex w-1/2 m-0 p-3"
-                  variants={variantsli}
-                >
-                  <Link href={rd.href}>
-                    <a className="mobile-link">{rd.name}</a>
-                  </Link>
-                </motion.li>
-              ))}
-          </Fragment>
-        ))}
-      </motion.ul>
-    </motion.div>
-  );
-};
 
 export default memo(LayoutHeader);
---
+++ components/MobileNav.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
+import { NavRoutes } from "interfaces/nav-routes";
+import { IntlFormatProps } from "@hooks/use-intl";
+
+type Props = {
+  isOpen: boolean;
+  navroutes: NavRoutes[];
+  f: (props: IntlFormatProps) => JSX.Element;
+};
+
+const MobileNav = ({ isOpen, navroutes, f }: Props) => {
+  const variants = {
+    open: {
+      transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+    },
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    },
+  };
+
+  const variantsli = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    },
+    closed: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 },
+      },
+    },
+  };
+  return (
+    <motion.div
+      className={clsx(
+        "bg-vulcan-800 text-gray-400 transition-all p-7 flex absolute top-12 left-0 flex-col items-center w-full z-20 opacity-0 pointer-events-none lg:hidden",
+        isOpen ? "opacity-100 pointer-events-auto" : ""
+      )}
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+    >
+      <h2 className="text-lg mb-5">Navigation</h2>
+      <motion.ul
+        className="flex items-center flex-row flex-wrap"
+        variants={variants}
+      >
+        {navroutes.map((r) => (
+          <Fragment key={r.name}>
+            <motion.li className="flex w-1/2 m-0 p-3" variants={variantsli}>
+              <Link href={r.href}>
+                <a className="mobile-link">
+                  {f({ id: r.id, defaultMessage: r.name })}
+                </a>
+              </Link>
+            </motion.li>
+            {r.dropdownMenu &&
+              r.dropdownMenu
+                .filter((d) => d.href !== "/#")
+                .map((rd) => (
+                  <motion.li
+                    key={rd.name}
+                    className="flex w-1/2 m-0 p-3"
+                    variants={variantsli}
+                  >
+                    <Link href={rd.href}>
+                      <a className="mobile-link">
+                        {f({ id: rd.id, defaultMessage: rd.name })}
+                      </a>
+                    </Link>
+                  </motion.li>
+                ))}
+          </Fragment>
+        ))}
+      </motion.ul>
+    </motion.div>
+  );
+};
+
+export default MobileNav;
