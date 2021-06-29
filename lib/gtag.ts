b19a35118b457266c7b4d92ff6657ee4176b4a5c
dvaJi <francisco_p@outlook.com>
2021-06-29 18:43:02 -0400

    feat: add kaedehara kazuha (info, build, and teams comp)

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,10 +1,12 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  (window as any).gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined" && (window as any).gtag) {
+    (window as any).gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
@@ -15,9 +17,11 @@ interface EventProps {
   value: string;
 }
 export const event = ({ action, category, label, value }: EventProps) => {
-  (window as any).gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (typeof window !== "undefined" && (window as any).gtag) {
+    (window as any).gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
