2bd5c87938e782c9fd242f0389b6682be0a8c2cd
dvaJi <francisco_p@outlook.com>
2023-01-26 01:26:19 -0300

    chore: update dependencies

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  organizeImportsSkipDestructiveCodeActions: true,
+  plugins: [
+    require("prettier-plugin-organize-imports"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+};
