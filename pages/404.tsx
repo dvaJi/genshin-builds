332f9d36db659ad0941c666e09c24f2279df93fd
dvaJi <francisco_p@outlook.com>
2023-01-30 20:32:32 -0300

    chore: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+function ErrorNotFound() {
+  return (
+    <div className="my-24">
+      <h1 className="text-center text-2xl">404 - Page Not Found</h1>
+    </div>
+  );
+}
+
+export default ErrorNotFound;
