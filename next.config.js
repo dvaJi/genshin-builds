224277a4fe786412de8d26f737704615032112a3
dvaJi <francisco_p@outlook.com>
2023-02-15 18:51:10 -0300

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -31,6 +31,8 @@ const nextConfig = {
   experimental: {
     legacyBrowsers: false,
   },
+  eslint: { ignoreDuringBuilds: !!process.env.CI },
+  typescript: { ignoreBuildErrors: !!process.env.CI },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
