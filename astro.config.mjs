33653ef8d5d804321b8f811cbcd5425002b4db28
dvaJi <francisco_p@outlook.com>
2023-07-10 11:20:57 -0400

    chore: add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,26 @@ import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
+import { i18n, defaultLocaleSitemapFilter } from "astro-i18n-aut";
+import sitemap from "@astrojs/sitemap";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-ES",
+  fr: "fr-CA",
+};
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    redirects: true,
+  },
+  site: "https://genshin-builds.com",
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
   integrations: [
     react(),
     tailwind(),
@@ -13,6 +30,17 @@ export default defineConfig({
       config: {
         forward: ["dataLayer.push"],
       },
+    }),
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: defaultLocaleSitemapFilter({ defaultLocale }),
     }),
   ],
   output: "static",
---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English",
+  fr: "Français",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+} as const;
