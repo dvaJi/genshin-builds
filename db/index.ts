a9eaed3c214512d2725b134da709db1145f5829a
dvaJi <francisco_p@outlook.com>
2023-11-15 19:34:15 -0300

    chore(genshin): update builds

--- db/index.ts
+++ db/index.ts
@@ -1,19 +1,18 @@
+import { createRequire } from "node:module";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
-import { createRequire } from "node:module";
 
-// Workaround for v5.6.0: https://github.com/prisma/prisma/discussions/21347?sort=new#discussioncomment-7576783
 const require = createRequire(import.meta.url);
 const { Client } = require("@planetscale/database");
+
+const client = new Client({ url:  process.env.DATABASE_URL  })
+const adapter = new PrismaPlanetScale(client);
+
+// Workaround for v5.6.0: https://github.com/prisma/prisma/discussions/21347?sort=new#discussioncomment-7576783
 
 export * from "@prisma/client";
 
 let prisma: PrismaClient;
-const client = new Client({
-  url: process.env.DATABASE_URL,
-  fetch,
-});
-const adapter = new PrismaPlanetScale(client);
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient({
