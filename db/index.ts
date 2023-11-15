0084c0616872d8c8d2628bd59324d9715dd2f86b
dvaJi <francisco_p@outlook.com>
2023-11-15 12:28:58 -0300

    fix: revert prisma to 5.5.2

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,12 @@
-import { Client } from '@planetscale/database'
+import { connect } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 export * from "@prisma/client";
 
 let prisma: PrismaClient;
-const client = new Client({ url: process.env.DATABASE_URL, fetch })
-const adapter = new PrismaPlanetScale(client);
+const connection = connect({ url: process.env.DATABASE_URL, fetch });
+const adapter = new PrismaPlanetScale(connection);
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient({
