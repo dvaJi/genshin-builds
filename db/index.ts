946b6ff25a343c20d8cecee30de55487bc58c89f
dvaJi <francisco_p@outlook.com>
2023-11-15 18:55:53 -0300

    fix: add workaround for prisma v5.6.0

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,19 @@
-import { connect } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
+import { createRequire } from "node:module";
+
+// Workaround for v5.6.0: https://github.com/prisma/prisma/discussions/21347?sort=new#discussioncomment-7576783
+const require = createRequire(import.meta.url);
+const { Client } = require("@planetscale/database");
 
 export * from "@prisma/client";
 
 let prisma: PrismaClient;
-const connection = connect({ url: process.env.DATABASE_URL, fetch });
-const adapter = new PrismaPlanetScale(connection);
+const client = new Client({
+  url: process.env.DATABASE_URL,
+  fetch,
+});
+const adapter = new PrismaPlanetScale(client);
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient({
