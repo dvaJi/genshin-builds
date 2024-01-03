a25184c2c2b4f4b717dd84ea0f3b8df166974137
dvaJi <francisco_p@outlook.com>
2024-01-02 23:06:27 -0300

    feat: Add profile not found component

---
+++ app/[lang]/(genshin)/profile/not-found.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Button from "@components/ui/Button";
+
+export default function NotFound() {
+  return (
+    <>
+      <div className="card relative z-20 overflow-hidden">
+        <Image
+          className="absolute -top-4 left-0 z-10 opacity-40 grayscale"
+          src="/imgs/stickers/sticker_10.png"
+          width={250}
+          height={400}
+          alt="Paimon (?)"
+        />
+        <div className="text-center">
+          <h2 className="mb-6 text-3xl text-zinc-200">Profile not found!</h2>
+          <p className="text-lg">
+            This profile currently does not exist. If you want to create it,
+            please submit your UID.
+            <br />
+            <Link href="/profile/">
+              <Button className="mt-6">Submit UID</Button>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
