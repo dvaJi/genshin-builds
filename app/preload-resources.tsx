88cdedbca13a704ea085f14ef48d993ad703b8a2
dvaJi <francisco_p@outlook.com>
2024-02-28 16:14:39 -0300

    chore: update dependencies

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -3,33 +3,45 @@
 import ReactDOM from "react-dom";
 
 export function PreloadResources() {
-  ReactDOM.preconnect("https://a.pub.network/", { crossOrigin: "anonymous" });
-  ReactDOM.preconnect("https://b.pub.network/", { crossOrigin: "anonymous" });
-  ReactDOM.preconnect("https://c.pub.network/", { crossOrigin: "anonymous" });
-  ReactDOM.preconnect("https://d.pub.network/", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://a.pub.network", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://b.pub.network", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://c.pub.network", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://d.pub.network", { crossOrigin: "anonymous" });
   ReactDOM.preconnect("https://c.amazon-adsystem.com", {
     crossOrigin: "anonymous",
   });
   ReactDOM.preconnect("https://s.amazon-adsystem.com", {
     crossOrigin: "anonymous",
   });
-  ReactDOM.preconnect("https://secure.quantserve.com/", {
+  ReactDOM.preconnect("https://secure.quantserve.com", {
     crossOrigin: "anonymous",
   });
-  ReactDOM.preconnect("https://rules.quantcount.com/", {
+  ReactDOM.preconnect("https://rules.quantcount.com", {
     crossOrigin: "anonymous",
   });
-  ReactDOM.preconnect("https://pixel.quantserve.com/", {
+  ReactDOM.preconnect("https://pixel.quantserve.com", {
     crossOrigin: "anonymous",
   });
-  ReactDOM.preconnect("https://cmp.quantcast.com/", {
+  ReactDOM.preconnect("https://cmp.quantcast.com", {
     crossOrigin: "anonymous",
   });
-  ReactDOM.preconnect("https://btloader.com/", { crossOrigin: "anonymous" });
-  ReactDOM.preconnect("https://api.btloader.com/", {
+  ReactDOM.preconnect("https://btloader.com", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://api.btloader.com", {
     crossOrigin: "anonymous",
   });
   ReactDOM.preconnect("https://confiant-integrations.global.ssl.fastly.net", {
+    crossOrigin: "anonymous",
+  });
+  ReactDOM.preconnect("https://securepubads.g.doubleclick.net", {
+    crossOrigin: "anonymous",
+  });
+  ReactDOM.preconnect("https://pagead2.googlesyndication.com", {
+    crossOrigin: "anonymous",
+  });
+  ReactDOM.preconnect("https://eus.rubiconproject.com", {
+    crossOrigin: "anonymous",
+  });
+  ReactDOM.preconnect("https://pixel.rubiconproject.com", {
     crossOrigin: "anonymous",
   });
 
