71ba18cc4c32340b91a052f0acaca1109e7ad925
dvaJi <francisco_p@outlook.com>
2024-03-13 15:27:38 -0300

    feat(genshin): update banner component

--- app/[lang]/(genshin)/banners.tsx
+++ app/[lang]/(genshin)/banners.tsx
@@ -20,7 +20,7 @@ type Props = {
 };
 
 export async function Banners({ lang }: Props) {
-  const timeline = await getRemoteData<Timeline[][]>("genshin", "timeline");
+  const timeline = await getRemoteData<Timeline[][]>("genshin", "timeline", 0);
 
   const now = new Date();
   const banners = timeline.flat().filter((t) => {
@@ -36,25 +36,28 @@ export async function Banners({ lang }: Props) {
   });
 
   return (
-    <div className="flex flex-wrap gap-4 p-4">
+    <div className="grid grid-cols-2 lg:grid-cols-4">
       {banners.map((banner) => (
         <Link
           href={`/${lang}/banners/${banner.name.endsWith("Weapon Banner") ? "weapons" : "characters"}`}
           key={banner.name}
-          className="group mx-auto max-w-md overflow-hidden p-2"
+          className="group mx-auto max-w-xs overflow-hidden"
         >
           <div className="relative aspect-video w-full overflow-hidden rounded bg-vulcan-900 object-cover">
             <img
-              src={getImg("genshin", `/events/${banner.image}`)}
+              src={getImg("genshin", `/events/${banner.image}`, {
+                width: 320,
+                height: 180,
+              })}
               alt={banner.name}
               className="aspect-video w-full rounded object-cover shadow-2xl transition-all group-hover:scale-105 group-hover:brightness-110"
             />
           </div>
           <div className="text-center">
-            <h3 className="mt-1 text-lg text-slate-300 transition-all group-hover:text-slate-100">
+            <h3 className="mt-1 text-base text-slate-300 transition-all group-hover:text-slate-100">
               {banner.name}
             </h3>
-            <p className="text-sm">
+            <p className="text-xs">
               {new Date(banner.start).toDateString()} -{" "}
               {new Date(banner.end).toDateString()}
             </p>
