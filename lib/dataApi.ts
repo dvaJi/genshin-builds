c3f03f26ff7df73ab0f089a44fec7cdc5328a566
dvaJi <francisco_p@outlook.com>
2024-05-08 07:52:23 -0400

    refactor: Add retry logic to getData function
    The getData function in dataApi.ts has been refactored to include retry logic. This change allows for a maximum of 3 retries when encountering a "Bad Gateway" error. The function will retry the request if the error occurs and the maximum number of retries has not been reached. If a different error occurs or the maximum retries are reached, the error will be thrown.

--- lib/dataApi.ts
+++ lib/dataApi.ts
@@ -19,25 +19,47 @@ export async function getHSRData<T>(options: APIOptions) {
   return getData<T>(HSR_API_URL, options, ["hsr-data"]);
 }
 
-async function getData<T>(url: string, options: APIOptions, tags: string[]) {
-  const res = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(options),
-    next: {
-      revalidate: options.revalidate ? options.revalidate : 60 * 60 * 24,
-      tags,
-    },
-  });
+async function getData<T>(
+  url: string,
+  options: APIOptions,
+  tags: string[]
+): Promise<T> {
+  const MAX_RETRIES = 3;
+  for (let i = 0; i < MAX_RETRIES; i++) {
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(options),
+        next: {
+          revalidate: options.revalidate ? options.revalidate : 60 * 60 * 24,
+          tags,
+        },
+      });
 
-  if (options.filter && res.status === 404) return null as T;
+      if (options.filter && res.status === 404) return null as T;
 
-  if (options.asMap) {
-    const data = await res.json();
-    return data.reduce((acc: any, curr: any) => {
-      acc[curr.id] = curr;
-      return acc;
-    }, {} as any) as T;
+      if (options.asMap) {
+        const data = await res.json();
+        return data.reduce((acc: any, curr: any) => {
+          acc[curr.id] = curr;
+          return acc;
+        }, {} as any) as T;
+      }
+
+      return res.json() as Promise<T>;
+    } catch (error) {
+      if (
+        error instanceof SyntaxError &&
+        error.message.includes("Bad Gateway") &&
+        i < MAX_RETRIES - 1
+      ) {
+        continue; // if it's a "Bad Gateway" error and we haven't reached the max retries, retry
+      } else {
+        throw error; // if it's a different error or we've reached the max retries, throw the error
+      }
+    }
   }
 
-  return res.json() as Promise<T>;
+  console.error("Max retries reached", options);
+  throw new Error("Max retries reached");
 }
