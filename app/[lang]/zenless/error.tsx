9707a6410d9d2a4312a6e68745d19e9b262e3590
dvaJi <francisco_p@outlook.com>
2024-10-02 20:13:43 -0300

    refactor: update error component styles in zenless

--- app/[lang]/zenless/error.tsx
+++ app/[lang]/zenless/error.tsx
@@ -18,7 +18,7 @@ export default function Error({ error, reset }: Props) {
   }, [error]);
 
   return (
-    <div className="relative z-20 overflow-hidden p-6">
+    <div className="relative z-20 overflow-hidden rounded-lg border-2 border-neutral-600 bg-neutral-800 p-6">
       <Image
         className="absolute -top-4 left-0 z-10 opacity-20 grayscale md:opacity-40 lg:opacity-50"
         src="/imgs/stickers/Sticker_Set_1_Nicole_smash.webp"
@@ -27,13 +27,15 @@ export default function Error({ error, reset }: Props) {
         alt="Nicole"
       />
       <div className="relative z-20 text-center">
-        <h2 className="mb-6 text-3xl">Something went wrong! {":("}</h2>
+        <h2 className="mb-6 text-3xl text-neutral-50">
+          Something went wrong! {":("}
+        </h2>
         <p className="text-lg">
           We&apos;re sorry, but an error has occurred. Please try again later.
           If the problem persists,{" "}
           <Link
             href="/contact"
-            className="text-slate-800 underline hover:text-slate-950"
+            className="text-yellow-500 underline hover:text-yellow-300"
             prefetch={false}
           >
             contact our support team
@@ -43,7 +45,7 @@ export default function Error({ error, reset }: Props) {
           <br />
           <br />
           <button
-            className="rounded-2xl border-2 border-black px-4 py-2 font-semibold ring-black transition-all hover:bg-white hover:ring-4"
+            className="rounded-2xl border-2 border-neutral-600 px-4 py-2 font-semibold ring-black transition-all hover:bg-neutral-600 hover:ring-4"
             onClick={
               // Attempt to recover by trying to re-render the segment
               async () => {
