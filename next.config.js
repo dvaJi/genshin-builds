70db91b79748a699f6b95dac614edca1b6bad135
dvaJi <francisco_p@outlook.com>
2024-10-08 13:36:03 -0300

    chore: update dependencies and comment out Sentry configuration options

--- next.config.js
+++ next.config.js
@@ -145,7 +145,7 @@ module.exports = withSentryConfig(module.exports, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  // tunnelRoute: "/monitoring",
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
@@ -157,5 +157,5 @@ module.exports = withSentryConfig(module.exports, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  // automaticVercelMonitors: true,
 });
