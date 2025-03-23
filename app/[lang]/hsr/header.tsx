adf90a5dc0ad821b3a2a4cafecdb82ad556fe91e
Francisco Pizarro <francisco_p@outlook.com>
2025-03-23 19:11:42 -0300

    fix: remove 'Tierlist' route from navigation

--- app/[lang]/hsr/header.tsx
+++ app/[lang]/hsr/header.tsx
@@ -15,7 +15,6 @@ const navRoutes: RouteType[] = [
   { id: "relics", name: "Relics", href: "/hsr/relics" },
   { id: "lightcones", name: "Light Cones", href: "/hsr/lightcones" },
   { id: "blog", name: "Blog", href: "/hsr/blog" },
-  { id: "tierlist", name: "Tierlist", href: "/hsr/tierlist" },
   { id: "items", name: "Items", href: "/hsr/item" },
   { id: "messages", name: "Messages", href: "/hsr/message" },
   { id: "achievements", name: "Achievements", href: "/hsr/achievements" },
